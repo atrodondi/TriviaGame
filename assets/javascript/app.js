9fae85ac7e71fd36e9fd02a66d78cc44d0e29307
cheebachong <atrodondi@gmail.com>
2020-02-19 14:09:18 -0800

    working on timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,30 +3,52 @@
 // make correct answer the same index as the question in first answer array, make the display of them random instead perfect.....:) ??
 
 var game = {
+  randomQuestObj: "",
+  currentQuestion: "",
+
   //when start button is pushed, the button is moved to a purgatory div to be hidden
-  startButton: function() {
-    $("#startButton").on("click", function() {
-      $("#startDiv").appendTo("#purgatory");
-      console.log("Start button pushed, and should b in purg");
-      //then we start populating an array with our question objects
-      game.questions.push(
-        game.question1,
-        game.question2,
-        game.question3,
-        game.question4,
-        game.question5
-      );
-      console.log(game.questions);
+  functions: {
+    startButton: function() {
+      $("#startButton").on("click", function() {
+        $("#startDiv").appendTo("#purgatory");
+        console.log("Start button pushed, and should b in purg");
+        game.functions.timer30();
 
-      //then we grab a random question object from the array we made
-      var randomQuestion =
-        game.questions[Math.floor(Math.random() * game.questions.length)];
-      var currentQuestion = randomQuestion.question;
+        //then we start populating an array with our question objects
+        game.questions.push(
+          game.question1,
+          game.question2,
+          game.question3,
+          game.question4,
+          game.question5
+        );
+        console.log(game.questions);
 
-      console.log(currentQuestion);
-      //and display the question to the user
-      $("#Q1").html("<p>" + currentQuestion + "</p>");
-    });
+        //then we grab a random question object from the array we made
+        game.randomQuestObj =
+          game.questions[Math.floor(Math.random() * game.questions.length)];
+        game.currentQuestion = game.randomQuestObj.question;
+
+        console.log(game.currentQuestion);
+        //and display the question to the user
+        $("#Q1").html("<p>" + game.currentQuestion + "</p>");
+      });
+    },
+
+    timer30: function() {
+      var timeLeft = 30;
+      var timerId = setInterval(count, 1000);
+
+      function count() {
+        if (timeLeft === -1) {
+          clearTimeout(timerId);
+          // make a  function elsewhere for unanswered html change, like show answer, picture, etc. then in 5 seconds change to a new question probably and call it here?
+        } else {
+          $("#timer").html("Time Remaining: " + timeLeft + " seconds");
+          timeLeft--;
+        }
+      }
+    }
   },
   questions: [],
 
@@ -56,4 +78,5 @@ var game = {
     wrongAnswers: ["Ronnie Van Zant", "Buzz Lightyear", "Tom Petty"]
   }
 };
-game.startButton();
+
+game.functions.startButton();
